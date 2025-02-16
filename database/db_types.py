3d972232f1fbc99c942a252e734831982d55a889
inmare <yatsumehole@naver.com>
2025-02-16 11:51:07 +0900

    데이터 베이스 타입을 별개의 파일로 분리

---
+++ database/db_types.py
@@ -0,0 +1,53 @@
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class Page(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    pageUrl: str  # 페이지 URL, 상대 경로
+    pageTitle: str  # 번역된 제목, 중복된 제목의 경우 작곡가가 제목에 포함될 수 있음
+    originalPageTitle: str | None = Field(default=None)
+    originalTitle: str  # 원 제목
+
+    songs: list["Song"] = Relationship(back_populates="page")
+    lyrics: list["Lyrics"] = Relationship(back_populates="page")
+
+
+# TODO: scrapy의 데이터 타입과 자동으로 연동되게 하기?
+class Song(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    page_id: int | None = Field(default=None, foreign_key="page.id")
+    page: Page | None = Relationship(back_populates="songs")
+
+    originalUrl: str | None = Field(default=None)  # 원곡 URL
+    # 곡에 참여한 사람, 음합엔
+    composer: str | None = Field(default=None)
+    lyricist: str
+    singer: str
+    originalSong: str | None = Field(default=None)
+    arranger: str | None = Field(default=None)
+    chorus: str | None = Field(default=None)
+    vocaloidEditor: str | None = Field(default=None)
+    illustrator: str | None = Field(default=None)
+    videoProducer: str | None = Field(default=None)
+    engineering: str | None = Field(default=None)
+    mixing: str | None = Field(default=None)
+    mastering: str | None = Field(default=None)
+    pianist: str | None = Field(default=None)
+    guitarist: str | None = Field(default=None)
+    bassist: str | None = Field(default=None)
+    drummer: str | None = Field(default=None)
+    trumpeter: str | None = Field(default=None)
+    trombonist: str | None = Field(default=None)
+    altoSaxophonist: str | None = Field(default=None)
+    playerEtc: str | None = Field(default=None)
+
+
+class Lyrics(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    page_id: int | None = Field(default=None, foreign_key="page.id")
+    page: Page | None = Relationship(back_populates="lyrics")
+
+    lyrics: str  # 가사
+    version: str | None = Field(default=None)
