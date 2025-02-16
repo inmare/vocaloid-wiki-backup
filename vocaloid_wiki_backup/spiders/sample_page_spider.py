dca1e6970f12a5c3be0f2e005327894018bd843f
inmare <yatsumehole@naver.com>
2025-02-16 11:37:18 +0900

    새로운 에러 링크 적용

--- vocaloid_wiki_backup/spiders/sample_page_spider.py
+++ vocaloid_wiki_backup/spiders/sample_page_spider.py
@@ -6,16 +6,26 @@ class VocaloidWikiSpider(scrapy.Spider):
     name = "sample-page"
 
     def start_requests(self):
+        # urls = [
+        #     "http://vocaro.wikidot.com/coaddiction",  # 일반적인 경우
+        #     "http://vocaro.wikidot.com/raise",  # 원본 링크가 없는 경우
+        #     "http://vocaro.wikidot.com/mosaic-roll",  # 2개의 곡과 1개의 표가 있는 경우
+        #     "http://vocaro.wikidot.com/dance-robot-dance",  # 2개의 곡과 1개의 표가 있는 경우
+        #     "http://vocaro.wikidot.com/alive",  # 제목이 중복이라서 링크가 있는 경우
+        #     "http://vocaro.wikidot.com/brand-new-day",  # 2개의 정보 표가 존재하는 경우
+        #     "http://vocaro.wikidot.com/telecaster-b-boy",  # 가사 표가 2개 이상인 경우
+        #     "http://vocaro.wikidot.com/immature-discipline",  # 가사 표 2개 중 1개가 잘못된 경우
+        #     "http://vocaro.wikidot.com/sing-a-song",  #  노래와 조교를 한 곳에 적어둔 경우
+        # ]
+
         urls = [
-            "http://vocaro.wikidot.com/coaddiction",
-            "http://vocaro.wikidot.com/raise",
-            "http://vocaro.wikidot.com/mosaic-roll",
-            "http://vocaro.wikidot.com/dance-robot-dance",
-            "http://vocaro.wikidot.com/alive",
-            "http://vocaro.wikidot.com/brand-new-day",
-            "http://vocaro.wikidot.com/telecaster-b-boy",
-            "http://vocaro.wikidot.com/immature-discipline",
-            "http://vocaro.wikidot.com/sing-a-song",
+            "http://vocaro.wikidot.com/t-a-o",  # 기타 참여자라는 숨겨진 표가 존재
+            "http://vocaro.wikidot.com/neppuu",  # 작사・작곡 형태
+            "http://vocaro.wikidot.com/eighty-eight",  # 작곡, 노래에 2명 이상의 사람이 존재
+            "http://vocaro.wikidot.com/super-turkish-march-doomed",  # 원곡이 존재, 링크가 없음
+            "http://vocaro.wikidot.com/momentary-drive",  # 출처 행이 없음
+            "http://vocaro.wikidot.com/turkish-march-doomed",  # 작곡이 없고 원곡만 존재
+            "http://vocaro.wikidot.com/the-rain-clear-up-twice",  # 표에서 가사가 따로 나뉨
         ]
 
         for url in urls:
