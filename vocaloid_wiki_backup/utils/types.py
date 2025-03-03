eea664d02ed508874b652108fc252b2f57f92843
inmare <yatsumehole@naver.com>
2025-03-03 19:27:15 +0900

    info-table 파싱 파일 위치 이동

--- vocaloid_wiki_backup/utils/types.py
+++ vocaloid_wiki_backup/utils/types.py
@@ -40,12 +40,6 @@ class PageInfo(TypedDict):
     lyricsInfo: list[LyricsInfo]
 
 
-class SongMetaInfo(TypedDict):
-    name: str
-    mustSame: bool
-    phrase: list[str]
-
-
 class SongTerm(TypedDict):
     name: str
     mustSame: bool
@@ -180,109 +174,5 @@ term_dictionary = [
         name="코러스 제작",
         mustSame=False,
         term=["코러스 제작"],
-    ),
-]
-
-
-SONG_META_INFO = [
-    SongMetaInfo(
-        name="composer",
-        mustSame=True,
-        phrase=["작곡", "작사작곡", "곡과 시"],
-    ),
-    SongMetaInfo(
-        name="lyricist",
-        mustSame=True,
-        phrase=["작사", "작사 협력", "작사작곡", "곡과 시"],
-    ),
-    SongMetaInfo(
-        name="singer",
-        mustSame=False,
-        phrase=["노래", "나레이션", "대사", "피처링", "삐뽀삐뽀"],
-    ),
-    SongMetaInfo(
-        name="originalSong",
-        mustSame=False,
-        phrase=["원곡"],
-    ),
-    SongMetaInfo(
-        name="arranger",
-        mustSame=False,
-        phrase=["편곡", "재편곡", "브라스 편곡", "피아노 편곡"],
-    ),
-    SongMetaInfo(
-        name="chorus",
-        mustSame=False,
-        phrase=["코러스", "보컬 서포트"],
-    ),
-    SongMetaInfo(
-        name="vocaloidEditor",
-        mustSame=False,
-        phrase=["조교", "음과 조교"],
-    ),
-    SongMetaInfo(
-        name="illustrator",
-        mustSame=False,
-        phrase=["일러스트", "그림", "그림과 영상"],
-    ),
-    SongMetaInfo(
-        name="videoProducer",
-        mustSame=False,
-        phrase=["영상", "그림과 영상"],
-    ),
-    SongMetaInfo(
-        name="engineering",
-        mustSame=False,
-        phrase=["엔지니어링", "브라스 녹음 엔지니어", "레코딩"],
-    ),
-    SongMetaInfo(
-        name="mixing",
-        mustSame=False,
-        phrase=["믹싱", "믹스", "보카로 믹싱 엔지니어"],
-    ),
-    SongMetaInfo(
-        name="mastering",
-        mustSame=False,
-        phrase=["마스터링"],
-    ),
-    SongMetaInfo(
-        name="pianist",
-        mustSame=False,
-        phrase=["피아노", "키보드", "신디사이저", "신스", "재즈피아노"],
-    ),
-    SongMetaInfo(
-        name="guitarist",
-        mustSame=False,
-        phrase=["기타", "기타 솔로"],
-    ),
-    SongMetaInfo(
-        name="bassist",
-        mustSame=False,
-        phrase=["베이스"],
-    ),
-    SongMetaInfo(
-        name="drummer",
-        mustSame=False,
-        phrase=["드럼"],
-    ),
-    SongMetaInfo(
-        name="trumpeter",
-        mustSame=False,
-        phrase=["트럼펫"],
-    ),
-    SongMetaInfo(
-        name="trombonist",
-        mustSame=False,
-        phrase=["트럼본"],
-    ),
-    SongMetaInfo(
-        name="altoSaxophonist",
-        mustSame=False,
-        phrase=["알토 색소폰"],
-    ),
-    SongMetaInfo(
-        name="playerEtc",
-        mustSame=False,
-        phrase=["연주", "하이라이트", "밴드", "스크래치", "코러스 제작"],
     ),
 ]
