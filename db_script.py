fc5f903e7cc8e6204873edbad2822b77a8f558aa
inmare <inmare2000@proton.me>
2025-03-04 22:54:51 +0900

    불필요한 코드 삭제

--- db_script.py
+++ db_script.py
@@ -1,4 +1,4 @@
-from database.create_db import (
+from vocaloid_wiki_backup.database.create_db import (
     create_db_and_tables,
     create_title,
     create_page,
@@ -14,16 +14,7 @@ def create_json_dir(delete_old_folder=False):
     os.makedirs("json/data")
 
 
-# import json
-
-# json_list = os.listdir("json/temp")
-# data_list = []
-# for json_file in json_list:
-#     with open(f"json/temp/{json_file}", "r", encoding="utf-8") as f:
-#         data = json.load(f)
-#         data_list.append(data)
-
-create_json_dir(delete_old_folder=True)
+# create_json_dir(delete_old_folder=True)
 create_db_and_tables(delete_old_db=True)
 create_title()
 
